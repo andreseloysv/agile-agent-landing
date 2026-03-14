485f0312406f17caa4437ae303ac7aba4b26dfea
Andres Eloy Sanchez Vargas <Andres-Eloy.A.Sanchez-Vargas-extern@deutschebahn.com>
2026-03-14 18:25:24 +0100

    update landing icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,7 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap' },
+        { rel: 'icon', type: 'image/png', href: '/icon.png' },
       ],
     },
   },
